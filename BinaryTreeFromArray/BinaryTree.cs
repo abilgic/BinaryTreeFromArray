e233b5dfe401d1737a9a7d9c5bbd0d281fa4f400
Ahmet Bilgiç <ahmet.bilgic@netone.net.tr>
2022-12-18 09:52:15 +0300

    Add project files.

---
+++ BinaryTreeFromArray/BinaryTree.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeFromArray
+{
+    public class BinaryTree
+    {
+        public Node Root;
+        public class Node
+        {
+            public int Data;
+            public Node left;
+            public Node right;
+
+            public Node(int data)
+            {
+                Data = data;
+            }
+        }
+
+            public Node BuildBinaryTree(int data, Node head)
+            {                
+                    if (head == null)
+                    {
+                        var newnode = new Node(data);
+                        head = newnode;
+                        return head;
+
+                    }
+                    else 
+                    {
+                        if (head.Data>data)
+                        {
+                            head.left = BuildBinaryTree(data, head.left);
+                        }
+                        else
+                        {
+                            head.right = BuildBinaryTree(data, head.right);
+
+                        }
+                        return head;
+
+                    }
+            }
+        public Node CreateBinaryTree(List<int> numbers, int start, int end)
+        {
+            if (start > end)
+                return null;
+
+            var mid = (start+end)/2;           
+            
+            var newnode = new Node(numbers[mid]);
+
+
+            newnode.left=CreateBinaryTree(numbers, start, mid-1);
+            newnode.right = CreateBinaryTree(numbers, mid+1,end);
+                return newnode;           
+
+        }
+        public void PreOrder(Node head)
+        {
+            var current = head;
+            if(current == null)
+            {
+                return;
+
+            }
+            Console.Write(current.Data+" ");
+            PreOrder(current.left);
+            PreOrder(head.right);
+        }
+
+    }
+
+                
+
+} 
---
+++ BinaryTreeFromArray/Program.cs
@@ -0,0 +1,15 @@
+﻿using BinaryTreeFromArray;
+using static BinaryTreeFromArray.BinaryTree;
+
+var binarytree = new BinaryTree();
+
+var numbers = new List<int>() {10,4,3,5,15,12 };
+
+//foreach (var number in numbers)
+//{
+//    binarytree.Root = binarytree.BuildBinaryTree(number, binarytree.Root);
+//}
+
+
+binarytree.Root = binarytree.CreateBinaryTree(numbers, 0, numbers.Count-1 );
+binarytree.PreOrder(binarytree.Root);
